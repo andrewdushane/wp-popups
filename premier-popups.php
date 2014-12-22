41eb19b083d887730200adb429756d047ca8daab
andy--d <andrew4craigslist@gmail.com>
2014-12-22 16:26:36 -0500

    Un-comment statements excluded for testing

--- premier-popups.php
+++ premier-popups.php
@@ -31,13 +31,13 @@ function premier_popup_content() {
 function premier_popups_scripts() {
 	if ( is_home() ) { //Only proceed on the homepage
 		$cookie = get_bloginfo('name');
-		//if( !isset($_COOKIE['premier-popups']) || $_COOKIE['premier-popups'] != $cookie) { //Only proceed if no cookie is set for this site
-			//setcookie('premier-popups', $cookie, time() + (86400 * 3) ); //Set cookie to expire in 3 days
+		if( !isset($_COOKIE['premier-popups']) || $_COOKIE['premier-popups'] != $cookie) { //Only proceed if no cookie is set for this site
+			setcookie('premier-popups', $cookie, time() + (86400 * 3) ); //Set cookie to expire in 3 days
 			wp_register_style( 'premier-popups', plugins_url( 'premier-popups/includes/style.css') );
 			wp_enqueue_style( 'premier-popups' );
 			wp_enqueue_script('premier-popups', plugins_url( 'premier-popups/includes/pop.js'), array( 'jquery', 'jquery-effects-core' ), '1.0', true);
 			add_action('wp_head', 'premier_popup_content');
-			//} //end cookie conditional
+		} //end cookie conditional
 	} //end home conditional
 } // end premier_popups_scripts
 add_action( 'wp_enqueue_scripts', 'premier_popups_scripts' );
